f9490991663e5bbec3ad5734ff3f7c691173e218
Kae Sluder <kaesluder@fastmail.com>
2025-06-01 21:46:18 -0400

    add title to pages

--- _includes/PageTemplate.jsx
+++ _includes/PageTemplate.jsx
@@ -10,6 +10,7 @@ export default (PageData, _helpers) => {
       <body className="prose prose-invert bg-stone-950 container mx-auto">
         <div className="flex flex-col items-center">
           <TopMenu />
+          <h1>{PageData.title}</h1>
           <main className="bg-stone-900 p-3 rounded">{PageData.children}</main>
           <Footer />
         </div>
